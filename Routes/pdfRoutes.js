36c25ce9fbe1d1015b6f52695912480630149c5f
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2025-05-24 14:20:29 -0300

    incluindo para salvar pdf e visualizar pelo QRcode

--- Routes/pdfRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateAndSavePdf } from '../controllers/pdfController.js';
-
-const router = express.Router();
-
-router.post('/generate-pdf', generateAndSavePdf);
-
-export { router as pdfRouter };
--- models/pdfModel.js
+++
@@ -1,8 +0,0 @@
-import con from '../utils/db.js';
-
-const savePdf = (codigo, pdfBuffer, callback) => {
-  const query = 'INSERT INTO certificados (codigo, pdf) VALUES (?, ?)';
-  con.query(query, [codigo, pdfBuffer], callback);
-};
-
-export { savePdf };
